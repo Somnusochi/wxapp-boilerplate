3906a071665b924d63c0439c2b4165beb1cfcebf
Somnusochi <tujunhong0014@gmail.com>
2017-07-27 11:58:08 +0800

    Update webpack.config.babel.js
    添加stylus支持，添加支持iconPath文件

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,10 +1,22 @@
-
-import { resolve } from 'path';
-import { DefinePlugin, EnvironmentPlugin, optimize } from 'webpack';
+import {
+	resolve
+}
+from 'path';
+import {
+	DefinePlugin,
+	EnvironmentPlugin,
+	optimize
+}
+from 'webpack';
 import WXAppWebpackPlugin from 'wxapp-webpack-plugin';
 import StylelintPlugin from 'stylelint-webpack-plugin';
+import CopyWebpackPlugin from 'copy-webpack-plugin';
 
-const { NODE_ENV, LINT, NO_LINT } = process.env;
+const {
+	NODE_ENV,
+	LINT,
+	NO_LINT
+} = process.env;
 const isDev = NODE_ENV !== 'production';
 const shouldLint = (!isDev || (!!LINT && LINT !== 'false')) && !NO_LINT;
 
@@ -22,100 +34,120 @@ export default {
 		path: resolve('dist'),
 	},
 	module: {
-		rules: [
-			{
+		rules: [{
 				test: /\.js$/,
 				include: /src/,
 				use: [
 					'babel-loader',
 					shouldLint && 'eslint-loader',
 				].filter(Boolean),
-			},
-			{
+			}, {
 				test: /\.json$/,
 				include: /src/,
-				use: [
-					{
-						loader: 'file-loader',
-						options: {
-							useRelativePath: true,
-							name: '[name].[ext]',
-						},
+				use: [{
+					loader: 'file-loader',
+					options: {
+						useRelativePath: true,
+						name: '[name].[ext]',
 					},
-				],
-			},
-			{
+				}, ],
+			}, {
 				test: /\.(png|jpg|gif)$/,
 				include: /src/,
 				loader: 'file-loader',
 				options: {
-					name: '/[name]_[hash:7].[ext]',
+					name: '/[name].[ext]',
 					outputPath: 'images',
 				}
-			},
-			{
-				test: /\.scss$/,
-				include: /src/,
-				use: [
-					{
-						loader: 'file-loader',
-						options: {
-							useRelativePath: true,
-							name: '[name].wxss',
-						}
-					},
-					{
-						loader: 'sass-loader',
-						options: {
-							includePaths: [
-								resolve('src', 'styles'),
-								resolve('src'),
-							],
-						},
-					},
-				],
-			},
-			{
+			}, {
 				test: /\.wxss$/,
 				include: /src/,
-				use: [
-					{
-						loader: 'file-loader',
-						options: {
-							useRelativePath: true,
-							name: '[name].wxss',
-						}
+				use: [{
+					loader: 'file-loader',
+					options: {
+						useRelativePath: true,
+						name: '[name].wxss',
+					}
+				}],
+			}, {
+				test: /\.scss$/,
+				include: /src/,
+				use: [{
+					loader: 'file-loader',
+					options: {
+						useRelativePath: true,
+						name: '[name].wxss',
+					}
+				}, {
+					loader: 'sass-loader',
+					options: {
+						includePaths: [
+							resolve('src', 'styles'),
+							resolve('src'),
+						],
 					},
-				],
-			},
-			{
+				}, ],
+			}, {
+				test: /\.less$/,
+				include: /src/,
+				use: [{
+					loader: 'file-loader',
+					options: {
+						useRelativePath: true,
+						name: '[name].wxss',
+					}
+				}, {
+					loader: 'less-loader',
+					options: {
+						includePaths: [
+							resolve('src', 'styles'),
+							resolve('src'),
+						],
+					},
+				}, ],
+			}, {
+				test: /\.styl$/,
+				include: /src/,
+				use: [{
+					loader: 'file-loader',
+					options: {
+						useRelativePath: true,
+						name: '[name].wxss',
+					}
+				}, {
+					loader: 'stylus-loader',
+					options: {
+						includePaths: [
+							resolve('src', 'styles'),
+							resolve('src'),
+						],
+					},
+				}, ],
+			}, {
 				test: /\.wxml$/,
 				include: resolve('src/pages'),
-				use: [
-					{
-						loader: 'file-loader',
-						options: {
-							useRelativePath: true,
-							name: '[name].[ext]',
-						},
+				use: [{
+					loader: 'file-loader',
+					options: {
+						useRelativePath: true,
+						name: '[name].[ext]',
 					},
-					{
-						loader: 'wxml-loader',
-						options: {
-							root: resolve('src'),
-						},
+				}, {
+					loader: 'wxml-loader',
+					options: {
+						root: resolve('src'),
 					},
-				],
+				}, ],
 			},
 			{
 				test: /\.wxml$/,
 				include: resolve('src'),
 				exclude: resolve('src/pages'),
-				use: [
-					{
+				use: [{
 						loader: 'file-loader',
 						options: {
-							name: '[name]_[hash:7].[ext]',
+							name: '/[name].[ext]',
+							outputPath: 'components',
 						},
 					},
 					{
@@ -125,10 +157,14 @@ export default {
 						},
 					},
 				],
-			},
+			}
 		],
 	},
 	plugins: [
+		new CopyWebpackPlugin([{
+			from: 'src/images',
+			to: 'images'
+		}]),
 		new EnvironmentPlugin({
 			NODE_ENV: 'development',
 		}),
@@ -148,3 +184,4 @@ export default {
 		aggregateTimeout: 300,
 	},
 };
+
